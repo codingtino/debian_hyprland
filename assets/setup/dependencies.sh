dc679463db573b2ff09c163ae7c7328f5cff31e2
t.naumann <t.naumann@gge-germany.de>
2024-11-20 17:34:36 +0100

    .

--- assets/setup/dependencies.sh
+++ assets/setup/dependencies.sh
@@ -8,8 +8,10 @@
 
 set -euo pipefail
 
-sudo apt-get update
-sudo apt-get -y full-upgrade
+#sudo apt-get update
+#sudo apt-get -y full-upgrade
+
+sudo apt-get -y install cmake
 
 mkdir -p $WORKING_DIR/tmp
 cd $WORKING_DIR/tmp
@@ -20,6 +22,7 @@ cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PRE
 cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
 sudo cmake --install build
 cd ..
+rm -r hyprutils/
 
 git clone https://github.com/hyprwm/hyprwayland-scanner.git
 cd hyprwayland-scanner/
@@ -27,6 +30,7 @@ cmake -DCMAKE_INSTALL_PREFIX=/usr -B build
 cmake --build build -j `nproc`
 sudo cmake --install build
 cd ..
+rm -r hyprwayland-scanner/
 
 git clone https://github.com/hyprwm/aquamarine.git
 cd aquamarine/
@@ -34,6 +38,7 @@ cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PRE
 cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
 sudo cmake --install build
 cd ..
+rm -r aquamarine/
 
 git clone https://github.com/hyprwm/hyprlang.git
 cd hyprlang/
@@ -41,6 +46,7 @@ cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PRE
 cmake --build ./build --config Release --target hyprlang -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
 sudo cmake --install build
 cd ..
+rm -r hyprlang/
 
 git clone https://github.com/hyprwm/hyprcursor.git
 cd hyprcursor/
@@ -48,11 +54,14 @@ cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PRE
 cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF` && \
 sudo cmake --install build
 cd ..
+rm -r hyprcursor/
 
 git clone --recursive https://github.com/hyprwm/Hyprland
 cd Hyprland
 make all && sudo make install
 cd ..
+rm -r Hyprland
+
 
 
 # cd $CALLER_DIR
