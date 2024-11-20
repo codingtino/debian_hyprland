6917633b4a65fa4f75ae488fd79eb038a188bc1a
t.naumann <t.naumann@gge-germany.de>
2024-11-20 17:26:07 +0100

    .

---
+++ assets/setup/dependencies.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+######################################################################
+# Title: Debian Hyprland                                             #
+# Author: Tino Naumann                                               #
+# Repository: https://github.com/codingtino/debian_hyprland          #
+######################################################################
+
+set -euo pipefail
+
+sudo apt-get update
+sudo apt-get -y full-upgrade
+
+mkdir -p $WORKING_DIR/tmp
+cd $WORKING_DIR/tmp
+
+git clone https://github.com/hyprwm/hyprutils.git
+cd hyprutils/
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
+cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
+sudo cmake --install build
+cd ..
+
+git clone https://github.com/hyprwm/hyprwayland-scanner.git
+cd hyprwayland-scanner/
+cmake -DCMAKE_INSTALL_PREFIX=/usr -B build
+cmake --build build -j `nproc`
+sudo cmake --install build
+cd ..
+
+git clone https://github.com/hyprwm/aquamarine.git
+cd aquamarine/
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
+cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
+sudo cmake --install build
+cd ..
+
+git clone https://github.com/hyprwm/hyprlang.git
+cd hyprlang/
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build
+cmake --build ./build --config Release --target hyprlang -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF`
+sudo cmake --install build
+cd ..
+
+git clone https://github.com/hyprwm/hyprcursor.git
+cd hyprcursor/
+cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && \
+cmake --build ./build --config Release --target all -j`nproc 2>/dev/null || getconf _NPROCESSORS_CONF` && \
+sudo cmake --install build
+cd ..
+
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+make all && sudo make install
+cd ..
+
+
+# cd $CALLER_DIR
---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+######################################################################
+# Title: Debian Hyprland                                             #
+# Author: Tino Naumann                                               #
+# Repository: https://github.com/codingtino/debian_hyprland          #
+######################################################################
+
+set -euo pipefail
+
+CALLER_DIR="$(pwd)"
+WORKING_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
+echo "Installing Dependencies"
+source "$WORKING_DIR/assets/setup/dependencies.sh"
+
+
+
+
+
+
+
