52cb907d9b568de33a04fc74234627d9d65a89ab
t.naumann <t.naumann@gge-germany.de>
2024-11-20 18:22:56 +0100

    .

--- assets/setup/dependencies.sh
+++ assets/setup/dependencies.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 #sudo apt-get update
 #sudo apt-get -y full-upgrade
 
-sudo apt-get -y install cmake build-essentials pkg-config
+sudo apt-get -y install cmake build-essential pkg-config
 
 mkdir -p $WORKING_DIR/tmp
 cd $WORKING_DIR/tmp
@@ -51,6 +51,7 @@ sudo cmake --install build
 cd ..
 rm -rf hyprlang/
 
+sudo apt-get -y install libzip-dev libcairo2-dev librsvg2-dev libtomlplusplus-dev
 git clone https://github.com/hyprwm/hyprcursor.git
 cd hyprcursor/
 cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_INSTALL_PREFIX:PATH=/usr -S . -B ./build && \
@@ -59,6 +60,7 @@ sudo cmake --install build
 cd ..
 rm -rf hyprcursor/
 
+sudo apt-get -y install libxkbcommon-dev libxcursor-dev 
 git clone --recursive https://github.com/hyprwm/Hyprland
 cd Hyprland
 make all && sudo make install
